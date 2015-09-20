f932ce8f9138423b419edef43a77de13d1998df9
David Dombrowsky <davek@6thstreetradio.org>
2015-09-19 21:58:18 -0400

    configured for linux

--- include/config.h
+++ include/config.h
@@ -169,11 +169,8 @@
 
 #ifdef UNIX
 /* path and file name extension for compression program */
-#define COMPRESS "/usr/bin/compress"	/* Lempel-Ziv compression */
-#define COMPRESS_EXTENSION ".Z"		/* compress's extension */
-/* An example of one alternative you might want to use: */
-/* #define COMPRESS "/usr/local/bin/gzip" */	/* FSF gzip compression */
-/* #define COMPRESS_EXTENSION ".gz" */		/* normal gzip extension */
+#define COMPRESS "/bin/gzip"
+#define COMPRESS_EXTENSION ".gz"
 #endif
 
 #ifndef COMPRESS
@@ -185,7 +182,7 @@
  *	a tar-like file, thus making a neater installation.  See *conf.h
  *	for detailed configuration.
  */
-/* #define DLB */	/* not supported on all platforms */
+#define DLB
 
 /*
  *	Defining INSURANCE slows down level changes, but allows games that
